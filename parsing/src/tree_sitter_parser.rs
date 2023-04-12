8275145804d3ddcd57564d0bd88e17346cd806e9
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-11 21:35:51 -0300

    refactor: extract parsing into a separate module

--- parsing/src/tree_sitter_parser.rs
+++ parsing/src/tree_sitter_parser.rs
@@ -1,16 +1,18 @@
 use model::Language;
 use std::collections::HashSet;
 
-pub struct TreeSitterParserConfiguration {
-    pub language: tree_sitter::Language,
-    pub stop_compilation_at: HashSet<&'static str>,
+pub struct ParserConfiguration {
+    pub(crate) language: tree_sitter::Language,
+    pub(crate) stop_compilation_at: HashSet<&'static str>,
 }
 
-pub fn from_language(language: Language) -> TreeSitterParserConfiguration {
-    match language {
-        Language::Java => TreeSitterParserConfiguration {
-            language: tree_sitter_java::language(),
-            stop_compilation_at: vec!["method_body"].into_iter().collect(),
-        },
+impl ParserConfiguration {
+    pub fn from_language(language: Language) -> ParserConfiguration {
+        match language {
+            Language::Java => ParserConfiguration {
+                language: tree_sitter_java::language(),
+                stop_compilation_at: vec!["method_body"].into_iter().collect(),
+            },
+        }
     }
 }
