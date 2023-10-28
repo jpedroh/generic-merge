c86742da116134287d375a1c03c1eb1955ec09b3
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-10-28 02:55:16 +0000

    refactor: Simplify entrypoint

--- model/src/cst_node.rs
+++ model/src/cst_node.rs
@@ -13,3 +13,20 @@ pub enum CSTNode<'a> {
         right: Box<Option<CSTNode<'a>>>,
     },
 }
+
+impl ToString for CSTNode<'_> {
+    fn to_string(&self) -> String {
+        match self {
+            CSTNode::Terminal { value, .. } => value.to_string(),
+            CSTNode::NonTerminal { children, .. } => {
+                children.iter().fold(String::new(), |acc, current| {
+                    let mut result = acc.to_owned();
+                    result.push_str(" ");
+                    result.push_str(&current.clone().to_string());
+                    result
+                })
+            }
+            CSTNode::Conflict { .. } => "Conflict found".into(),
+        }
+    }
+}
