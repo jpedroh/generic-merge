f7c1063b9ac11062beeab5775796e742e5cb67a1
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-04 18:40:51 -0300

    chore: Remove unused file

--- bin/tests/samples/smoke_java/merge.output.java
+++
@@ -1,1 +0,0 @@
-   public class Main  {  void delete  (  Pessoa pessoa ) ;  void create  (  Pessoa pessoa ) ;   public static void main  (   String  [ ] args )  {   int  x = 0 ;    System . out . println  ( <<<<<<<<< "Hello, João!" ========= "Hello, Paulo!" >>>>>>>>> ) ;   int  y = <<<<<<<<< 3 ========= 5 >>>>>>>>> ; }  void upsert  (  Pessoa pessoa ) ; }
