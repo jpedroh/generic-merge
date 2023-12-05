63a80513f83b04ea8b4470a6eb6227a184c2e3ce
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-05 01:01:31 -0300

    test: Fix incorrect path

--- bin/tests/cli.rs
+++ bin/tests/cli.rs
@@ -5,10 +5,10 @@ use assert_cmd::prelude::*;
 #[test]
 fn if_there_is_a_conflict_it_returns_valid_exit_code() {
     let mut cmd = Command::cargo_bin("generic-merge").unwrap();
-    cmd.arg("--base-path=tests/samples/smoke_java/base.java")
-        .arg("--left-path=tests/samples/smoke_java/left.java")
-        .arg("--right-path=tests/samples/smoke_java/right.java")
-        .arg("--merge-path=tests/samples/smoke_java/merge.java")
+    cmd.arg("--base-path=tests/scenarios/smoke_java/base.java")
+        .arg("--left-path=tests/scenarios/smoke_java/left.java")
+        .arg("--right-path=tests/scenarios/smoke_java/right.java")
+        .arg("--merge-path=tests/scenarios/smoke_java/merge.java")
         .assert()
         .code(bin::SUCCESS_WITH_CONFLICTS);
 }
@@ -16,10 +16,10 @@ fn if_there_is_a_conflict_it_returns_valid_exit_code() {
 #[test]
 fn if_there_is_no_conflict_it_returns_valid_exit_code() {
     let mut cmd = Command::cargo_bin("generic-merge").unwrap();
-    cmd.arg("--base-path=tests/samples/no_conflicts/base.java")
-        .arg("--left-path=tests/samples/no_conflicts/left.java")
-        .arg("--right-path=tests/samples/no_conflicts/right.java")
-        .arg("--merge-path=tests/samples/no_conflicts/merge.java")
+    cmd.arg("--base-path=tests/scenarios/no_conflicts/base.java")
+        .arg("--left-path=tests/scenarios/no_conflicts/left.java")
+        .arg("--right-path=tests/scenarios/no_conflicts/right.java")
+        .arg("--merge-path=tests/scenarios/no_conflicts/merge.java")
         .assert()
         .code(bin::SUCCESS_WITHOUT_CONFLICTS);
 }
