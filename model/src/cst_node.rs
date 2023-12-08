b8f12891fd2e9574fbf7ff75bd8ed5f413bf1fbd
Joao Duarte <joao.pedro.hsd@gmail.com>
2023-12-07 21:02:46 -0300

    refactor: Remove custom Hash implementations

--- model/src/cst_node.rs
+++ model/src/cst_node.rs
@@ -1,41 +1,18 @@
-use std::hash::{Hash, Hasher};
+use std::hash::Hash;
 
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Point {
     pub row: usize,
     pub column: usize,
 }
 
-impl Hash for Point {
-    fn hash<H: Hasher>(&self, _hasher: &mut H) {
-        let mut hasher = std::collections::hash_map::DefaultHasher::new();
-        self.row.hash(&mut hasher);
-        self.column.hash(&mut hasher);
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord, Hash)]
 pub enum CSTNode<'a> {
     Terminal(Terminal<'a>),
     NonTerminal(NonTerminal<'a>),
 }
 
-impl<'a> Hash for CSTNode<'a> {
-    fn hash<H: Hasher>(&self, _hasher: &mut H) {
-        let mut hasher = std::collections::hash_map::DefaultHasher::new();
-
-        match self {
-            CSTNode::Terminal(node) => {
-                node.hash(&mut hasher);
-            }
-            CSTNode::NonTerminal(node) => {
-                node.hash(&mut hasher);
-            }
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord, Hash)]
 pub struct NonTerminal<'a> {
     pub kind: &'a str,
     pub children: Vec<CSTNode<'a>>,
@@ -44,32 +21,10 @@ pub struct NonTerminal<'a> {
     pub are_children_unordered: bool,
 }
 
-impl<'a> Hash for NonTerminal<'a> {
-    fn hash<H: Hasher>(&self, _hasher: &mut H) {
-        let mut hasher = std::collections::hash_map::DefaultHasher::new();
-
-        self.kind.hash(&mut hasher);
-        self.children.hash(&mut hasher);
-        self.start_position.hash(&mut hasher);
-        self.end_position.hash(&mut hasher);
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord, Hash)]
 pub struct Terminal<'a> {
     pub kind: &'a str,
     pub value: &'a str,
     pub start_position: Point,
     pub end_position: Point,
-}
-
-impl<'a> Hash for Terminal<'a> {
-    fn hash<H: Hasher>(&self, _hasher: &mut H) {
-        let mut hasher = std::collections::hash_map::DefaultHasher::new();
-
-        self.kind.hash(&mut hasher);
-        self.value.hash(&mut hasher);
-        self.start_position.hash(&mut hasher);
-        self.end_position.hash(&mut hasher);
-    }
 }
