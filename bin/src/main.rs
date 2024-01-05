07f33c1d394a7ff9baa36d5acc254a2102bf85df
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-01-04 21:59:31 -0300

    feat: Log errors on failure

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -8,21 +8,34 @@ use clap::Parser;
 fn main() {
     let args = cli_args::CliArgs::parse();
 
-    let base = std::fs::read_to_string(&args.base_path)
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::READING_FILE_ERROR));
-    let left = std::fs::read_to_string(args.left_path)
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::READING_FILE_ERROR));
-    let right = std::fs::read_to_string(args.right_path)
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::READING_FILE_ERROR));
+    let base = std::fs::read_to_string(&args.base_path).unwrap_or_else(|error| {
+        println!("Error reading file: {}", error);
+        std::process::exit(cli_exit_codes::READING_FILE_ERROR)
+    });
+    let left = std::fs::read_to_string(args.left_path).unwrap_or_else(|error| {
+        println!("Error reading file: {}", error);
+        std::process::exit(cli_exit_codes::READING_FILE_ERROR)
+    });
+    let right = std::fs::read_to_string(args.right_path).unwrap_or_else(|error| {
+        println!("Error reading file: {}", error);
+        std::process::exit(cli_exit_codes::READING_FILE_ERROR)
+    });
 
-    let language = language::get_language_by_file_path(&args.base_path)
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::GUESS_LANGUAGE_ERROR));
+    let language = language::get_language_by_file_path(&args.base_path).unwrap_or_else(|error| {
+        println!("Error guessing language: {}", error);
+        std::process::exit(cli_exit_codes::GUESS_LANGUAGE_ERROR)
+    });
 
     let result = control::run_tool_on_merge_scenario(language, &base, &left, &right)
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::INTERNAL_EXECUTION_ERROR));
+        .unwrap_or_else(|error| {
+            println!("Error running tool: {}", error);
+            std::process::exit(cli_exit_codes::INTERNAL_EXECUTION_ERROR)
+        });
 
-    std::fs::write(args.merge_path, result.to_string())
-        .unwrap_or_else(|_| std::process::exit(cli_exit_codes::WRITING_FILE_ERROR));
+    std::fs::write(args.merge_path, result.to_string()).unwrap_or_else(|error| {
+        println!("Error writing file: {}", error);
+        std::process::exit(cli_exit_codes::WRITING_FILE_ERROR)
+    });
 
     match result {
         control::ExecutionResult::WithConflicts(_) => {
