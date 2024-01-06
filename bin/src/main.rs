23f1b4bbb79c40701aa2cc633f0119484cb1e373
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-01-06 20:32:51 -0300

    chore: Add structured logging

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -6,42 +6,46 @@ mod language;
 use clap::Parser;
 
 fn main() {
+    env_logger::init();
+
     let args = cli_args::CliArgs::parse();
 
     let base = std::fs::read_to_string(&args.base_path).unwrap_or_else(|error| {
-        println!("Error reading file: {}", error);
+        log::error!("Error while reading base file: {}", error);
         std::process::exit(cli_exit_codes::READING_FILE_ERROR)
     });
-    let left = std::fs::read_to_string(args.left_path).unwrap_or_else(|error| {
-        println!("Error reading file: {}", error);
+    let left = std::fs::read_to_string(&args.left_path).unwrap_or_else(|error| {
+        log::error!("Error while reading left file: {}", error);
         std::process::exit(cli_exit_codes::READING_FILE_ERROR)
     });
-    let right = std::fs::read_to_string(args.right_path).unwrap_or_else(|error| {
-        println!("Error reading file: {}", error);
+    let right = std::fs::read_to_string(&args.right_path).unwrap_or_else(|error| {
+        log::error!("Error while reading right file: {}", error);
         std::process::exit(cli_exit_codes::READING_FILE_ERROR)
     });
 
     let language = language::get_language_by_file_path(&args.base_path).unwrap_or_else(|error| {
-        println!("Error guessing language: {}", error);
+        log::error!("Error while guessing language: {}", error);
         std::process::exit(cli_exit_codes::GUESS_LANGUAGE_ERROR)
     });
 
     let result = control::run_tool_on_merge_scenario(language, &base, &left, &right)
         .unwrap_or_else(|error| {
-            println!("Error running tool: {}", error);
+            log::error!("Error while running tool: {}", error);
             std::process::exit(cli_exit_codes::INTERNAL_EXECUTION_ERROR)
         });
 
     std::fs::write(args.merge_path, result.to_string()).unwrap_or_else(|error| {
-        println!("Error writing file: {}", error);
+        log::error!("Error while writing output file: {}", error);
         std::process::exit(cli_exit_codes::WRITING_FILE_ERROR)
     });
 
     match result {
         control::ExecutionResult::WithConflicts(_) => {
+            log::info!("Execution finished with conflicts");
             std::process::exit(cli_exit_codes::SUCCESS_WITH_CONFLICTS)
         }
         control::ExecutionResult::WithoutConflicts(_) => {
+            log::info!("Execution finished without conflicts");
             std::process::exit(cli_exit_codes::SUCCESS_WITHOUT_CONFLICTS)
         }
     }
