81a4d6a853546d3d85765fa26060f94180b06fbc
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-03-02 00:01:42 -0300

    test: Add tests for the new handlers

--- matching_handlers/src/java/import_declaration.rs
+++ matching_handlers/src/java/import_declaration.rs
@@ -27,3 +27,44 @@ pub fn compute_matching_score_for_import_declaration<'a>(
         (_, _) => 0,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn imports_of_the_same_resource_matches_with_one() {
+        let result = super::compute_matching_score_for_import_declaration(
+            &make_import_of_resource("java.util.array"),
+            &make_import_of_resource("java.util.array"),
+        );
+        assert_eq!(1, result);
+    }
+
+    #[test]
+    fn imports_of_different_resources_matches_with_zero() {
+        let result = super::compute_matching_score_for_import_declaration(
+            &make_import_of_resource("java.util.array"),
+            &make_import_of_resource("java.util.list"),
+        );
+        assert_eq!(0, result);
+    }
+
+    fn make_import_of_resource(resource: &str) -> model::CSTNode {
+        model::CSTNode::NonTerminal(model::cst_node::NonTerminal {
+            kind: "import_declaration",
+            children: vec![
+                model::CSTNode::NonTerminal(model::cst_node::NonTerminal {
+                    kind: "scoped_identifier",
+                    children: resource.split(".").map(|part| {
+                        model::CSTNode::Terminal(model::cst_node::Terminal {
+                            kind: "identifier",
+                            value: part,
+                            ..Default::default()
+                        })
+                    }).collect(),
+                    ..Default::default()
+                })
+            ],
+            ..Default::default()
+        })
+    }
+}
