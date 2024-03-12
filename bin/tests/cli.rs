c9690619aecfa0ad1243e0b3bef7e0fdf511d887
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-03-11 22:20:20 -0300

    test: Uncomment tests

--- bin/tests/cli.rs
+++ bin/tests/cli.rs
@@ -2,26 +2,26 @@ use std::process::Command;
 
 use assert_cmd::prelude::*;
 
-// #[test]
-// fn if_there_is_a_conflict_it_returns_valid_exit_code() {
-//     let mut cmd = Command::cargo_bin("generic-merge").unwrap();
-//     cmd.arg("--base-path=tests/scenarios/smoke_java/base.java")
-//         .arg("--left-path=tests/scenarios/smoke_java/left.java")
-//         .arg("--right-path=tests/scenarios/smoke_java/right.java")
-//         .arg("--merge-path=tests/scenarios/smoke_java/merge.output.java")
-//         .arg("--language=java")
-//         .assert()
-//         .code(bin::SUCCESS_WITH_CONFLICTS);
-// }
-//
-// #[test]
-// fn if_there_is_no_conflict_it_returns_valid_exit_code() {
-//     let mut cmd = Command::cargo_bin("generic-merge").unwrap();
-//     cmd.arg("--base-path=tests/scenarios/no_conflicts/base.java")
-//         .arg("--left-path=tests/scenarios/no_conflicts/left.java")
-//         .arg("--right-path=tests/scenarios/no_conflicts/right.java")
-//         .arg("--merge-path=tests/scenarios/no_conflicts/merge.output.java")
-//         .arg("--language=java")
-//         .assert()
-//         .code(bin::SUCCESS_WITHOUT_CONFLICTS);
-// }
+#[test]
+fn if_there_is_a_conflict_it_returns_valid_exit_code() {
+    let mut cmd = Command::cargo_bin("generic-merge").unwrap();
+    cmd.arg("--base-path=tests/scenarios/smoke_java/base.java")
+        .arg("--left-path=tests/scenarios/smoke_java/left.java")
+        .arg("--right-path=tests/scenarios/smoke_java/right.java")
+        .arg("--merge-path=tests/scenarios/smoke_java/merge.output.java")
+        .arg("--language=java")
+        .assert()
+        .code(bin::SUCCESS_WITH_CONFLICTS);
+}
+
+#[test]
+fn if_there_is_no_conflict_it_returns_valid_exit_code() {
+    let mut cmd = Command::cargo_bin("generic-merge").unwrap();
+    cmd.arg("--base-path=tests/scenarios/no_conflicts/base.java")
+        .arg("--left-path=tests/scenarios/no_conflicts/left.java")
+        .arg("--right-path=tests/scenarios/no_conflicts/right.java")
+        .arg("--merge-path=tests/scenarios/no_conflicts/merge.output.java")
+        .arg("--language=java")
+        .assert()
+        .code(bin::SUCCESS_WITHOUT_CONFLICTS);
+}
